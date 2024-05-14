66bb007f1411d6e7850eb71268247289500e0be3
Diegodenac <diegocanedo917@gmail.com>
2024-05-13 23:00:20 -0400

    refact: changing names of variables and the condition to identify the string can't be transform into a single number

--- src/strCalculator.js
+++ src/strCalculator.js
@@ -1,7 +1,8 @@
 function stringCalculator(string) {
-  if(Number(string) || Number(string)===0) return Number(string);
-  let numbers = string.split(',');
-  return Number(numbers[0])+Number(numbers[1]);
+  let string_to_a_number = Number(string);
+  if(!isNaN(string_to_a_number)) return string_to_a_number;
+  let numbers_in_string = string.split(',');
+  return Number(numbers_in_string[0])+Number(numbers_in_string[1]);
 }
 
 export default stringCalculator;

5711448b78d03a643264aed0971d48a28d0c7c39
Diegodenac <diegocanedo917@gmail.com>
2024-05-14 00:06:28 -0400

    refact: making a function for splitNumbersOfString which returns an array of just numbers

--- src/strCalculator.js
+++ src/strCalculator.js
@@ -1,18 +1,28 @@
 function stringCalculator(string) {
   let string_to_a_number = Number(string);
-  if(!isNaN(string_to_a_number)) return string_to_a_number;
+  if(isNaN(string_to_a_number)) {
+    let sum = 0;
+    let arrayOfNumbers = splitNumbersOfString(string);
+    for (const number of arrayOfNumbers)
+      sum+= number;
+    return sum;
+  }
+  else return string_to_a_number;
+}
+
+function splitNumbersOfString(string){
   const elements_split_by_comas = string.split(',');
-  let sum = 0;
+  let arrayOfNumbers = [];
   for (const element_comas of elements_split_by_comas){
     if(isNaN(Number(element_comas))){
       const elements_split_by_hyphens = element_comas.split('-');
       for (const element_hyphens of elements_split_by_hyphens){
-        sum+= Number(element_hyphens);
+        arrayOfNumbers.push(Number(element_hyphens));
       }
     }
-    else sum += Number(element_comas);
+    else arrayOfNumbers.push(Number(element_comas));
   }
-  return sum;
+  return arrayOfNumbers;
 }
 
 export default stringCalculator;
